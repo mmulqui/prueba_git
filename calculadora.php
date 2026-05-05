2fe73fb893ce3254e2541ba0dca39b6ebeca7998
MigueSSJ247 <miguelangelromero2o1553@gmail.com>
2026-05-05 20:13:30 -0300

    (feat):se añade funciones nuevas

--- calculadora.php
+++ calculadora.php
@@ -7,17 +7,17 @@ function sumar(int $a, int $b): int {
     return $a + $b;
 }
 
-#function restar(int $a, int $b): int {
-#    return $a - $b;
-#}
+function restar(int $a, int $b): int {
+    return $a - $b;
+}
 
-#function multiplicar(int $a, int $b): int {
-#    return $a * $b;
-#}
+function multiplicar(int $a, int $b): int {
+    return $a * $b;
+}
 
-#function dividir(int $a, int $b): float {
-#    if ($b === 0) {
-#        throw new InvalidArgumentException("No se puede dividir entre cero.");
-#    }
-#    return $a / $b;
-#}
+function dividir(int $a, int $b): float {
+    if ($b === 0) {
+        throw new InvalidArgumentException("No se puede dividir entre cero.");
+    }
+    return $a / $b;
+}
